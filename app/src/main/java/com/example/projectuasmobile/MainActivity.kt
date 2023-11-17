352b768ed09bc42b8f762d61359607fde68c3c4c
iniryan <riyanadi552@gmail.com>
2023-11-17 09:52:41 +0700

    add login

--- app/src/main/java/com/example/projectuasmobile/MainActivity.kt
+++ app/src/main/java/com/example/projectuasmobile/MainActivity.kt
@@ -3,15 +3,44 @@ package com.example.projectuasmobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.projectuasmobile.ui.theme.ProjectUASMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,31 +53,191 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Login()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    OutlinedButton(onClick = { }) {
-     Text(text = "test")
-    }
-    Button(onClick = {  }) {
-        Text(text = "test")
-    }
-}
+fun Login() {
+    val usernameField = remember { mutableStateOf(TextFieldValue("")) }
+    val passwordField = remember { mutableStateOf(TextFieldValue("")) }
+    val passwordVisible = remember { mutableStateOf(false) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectUASMobileTheme {
-        Greeting("Android")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = "SDK-app",
+                style = TextStyle(
+                    fontSize = 48.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                    textAlign = TextAlign.Left
+                ),
+                modifier = Modifier.align(Alignment.Start)
+            )
+            Text(
+                text = "Memudahkan untuk order anda",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    color = Color(0xFF1E1E1E),
+                    textAlign = TextAlign.Left
+                ),
+                modifier = Modifier.align(Alignment.Start)
+            )
+            Text(
+                text = "Email",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    color = Color(0xFF1E1E1E),
+                    textAlign = TextAlign.Center,
+                ),
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(top = 48.dp)
+            )
+            OutlinedTextField(
+                value = usernameField.value,
+                onValueChange = {
+                    usernameField.value = it
+                },
+                singleLine = true,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .fillMaxWidth()
+                    .padding(2.dp)
+                    .border(
+                        width = 1.5.dp,
+                        color = Color(0xFF6650a4),
+                        shape = RoundedCornerShape(8.dp)
+                    ),
+                placeholder = { Text(text = "Contoh: example@test.com") }
+            )
+            Text(
+                text = "Password",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    color = Color(0xFF1E1E1E),
+                    textAlign = TextAlign.Center,
+                ),
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(top = 28.dp)
+            )
+            OutlinedTextField(
+                value = passwordField.value,
+                onValueChange = {
+                    passwordField.value = it
+                },
+                singleLine = true,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .fillMaxWidth()
+                    .padding(2.dp)
+                    .border(
+                        width = 1.5.dp,
+                        color = Color(0xFF6650a4),
+                        shape = RoundedCornerShape(8.dp)
+                    ),
+                placeholder = { Text(text = "Masukkan password") },
+                visualTransformation =
+                if (passwordVisible.value)
+                    VisualTransformation.None
+                else
+                    PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                trailingIcon = {
+                    IconButton(
+                        onClick = { passwordVisible.value = !passwordVisible.value },
+                        modifier = Modifier.padding(end = 10.dp)
+                    ) {
+                        Icon(
+                            painter =
+                            if (passwordVisible.value)
+                                painterResource(id = R.drawable.eye_open)
+                            else
+                                painterResource(id = R.drawable.eye_close),
+                            contentDescription = "Toggle Password"
+                        )
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.padding(20.dp))
+            ElevatedButton(
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .fillMaxWidth()
+                    .padding(2.dp)
+                    .height(48.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White,
+                ),
+                shape = RoundedCornerShape(8.dp),
+                onClick = {
+
+                }
+            )
+            {
+                Text(
+                    text = "Mulai Berbagi",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_semibold)),
+                        color = Color.White,
+                        textAlign = TextAlign.Center,
+                    )
+                )
+            }
+            Text(
+                text = "atau masuk dengan",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    color = Color(0xFF1E1E1E),
+                    textAlign = TextAlign.Center,
+                ),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 14.dp)
+                    .padding(bottom = 28.dp)
+            )
+            Row {
+                Text(
+                    text = "Belum punya akun?",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                        color = Color(0xFF1E1E1E),
+                        textAlign = TextAlign.Left
+                    ),
+                    modifier = Modifier.padding(top = 48.dp)
+                )
+                Spacer(modifier = Modifier.padding(4.dp))
+                ClickableText(
+                    text = AnnotatedString("Daftar dulu"),
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.poppins_medium)),
+                        color = Color(0xFF6650a4),
+                        textAlign = TextAlign.Left
+                    ),
+                    modifier = Modifier.padding(top = 48.dp)
+                ) {
+                    //navController.navigate("register")
+                }
+            }
+
+        }
     }
 }
