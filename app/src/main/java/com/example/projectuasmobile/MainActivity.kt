fc67f52137a548b478ba8719dc2f41fe9c8c3503
abrielkarisma <101442361+abrielkarisma@users.noreply.github.com>
2023-11-17 09:44:38 +0700

    theme color

--- app/src/main/java/com/example/projectuasmobile/MainActivity.kt
+++ app/src/main/java/com/example/projectuasmobile/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,6 +37,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    OutlinedButton(onClick = { }) {
+     Text(text = "test")
+    }
+    Button(onClick = {  }) {
+        Text(text = "test")
+    }
 }
 
 @Preview(showBackground = true)
